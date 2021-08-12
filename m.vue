3960490945d656a52b0684706fa62840b968536c
zhanghong-dev <zhanghong@vip.163.com>
2021-08-12 15:55:56 +0800

    Add files via upload

---
+++ m.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="portal-component__input-option-group">
+
+        <div class="at__header">
+            {{ label }}
+        </div>
+
+        <div class="at__body">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+
+        props: {
+
+            label: {
+                type: String
+            }
+
+        },
+
+        data() {
+
+            return {};
+
+        }
+
+    }
+
+</script>
